9ab5a8a866b0b1638d97ee1dc8ef3698d63e792b
adriano <adriano.gratani1@gmail.com>
2024-12-02 13:09:31 +0900

    *res -fixed footer mobile layout

--- src/components/FooterMain.js
+++ src/components/FooterMain.js
@@ -53,12 +53,15 @@ function FooterTextAnimation({textIn, intrvl}) {
 
 export default function FooterMain() {
     // if you don't desctructure context data as obj, it will return the whole obj.
-    const {screenMode} = useScreenContext();
+    const {screenMode, es} = useScreenContext();
     const ev=(s)=> {
         if (s==='large') {return "desktop"};
         return s;
     }
-    const animStr = `  Welcome to the ${ev(screenMode)} version for my Website.        `
+
+    let animStr = es() !== 'mobile' 
+        ? `  Welcome to the ${ev(screenMode)} version for my Website.        ` 
+        :  `  Welcome to my Mobile Website.        `;
 
     return (
 // {/*  A PICTURE MAYBE?  */}
@@ -81,17 +84,24 @@ export default function FooterMain() {
                     </div>
                 </div>
 
-                <div className="footer_header_right playfair-display_light">
-                    <div className="footer_header_aboutme link">
-                        <a href="#">My Journey</a>
-                    </div>
-                    <div className="footer_header_method link">
-                        <a href="#">Work Ethic</a>
-                    </div>
-                    <div className="footer_header_code link">
-                        <a href="#">My Code</a>
-                    </div>
-                </div>
+                {
+                    es() !== 'mobile'
+                    ?
+                        <div className="footer_header_right playfair-display_light">
+                            <div className="footer_header_aboutme link">
+                                <a href="#">My Journey</a>
+                            </div>
+                            <div className="footer_header_method link">
+                                <a href="#">Work Ethic</a>
+                            </div>
+                            <div className="footer_header_code link">
+                                <a href="#">My Code</a>
+                            </div>
+                        </div>
+                    
+                    :   null
+                }
+
             </div>
 
             <div class="footer_animation_container playfair-display_light">
@@ -104,7 +114,11 @@ export default function FooterMain() {
                 
                 <div className="footer_form_container">
                     <div className="footer_form_description_container playfair-display_light">
-                        Contact me for further discussion about Frontend development
+                        {
+                            es() !== 'mobile' ? 
+                            "Contact me for further discussion about Frontend development"
+                            : "Send me an e-mail:"
+                        }
                     </div>
                     <div className="footer_form_container">
 
@@ -128,11 +142,17 @@ export default function FooterMain() {
                         <a href="#" class="link playfair-display_light">Youtube</a>
                         <a href="#" class="link playfair-display_light">CV</a>
                         
-                        <div className="footer_signature_container">
-                            <div className="footer_signature playfair-display_light">
-                                Adriano Gratani
-                            </div>
-                        </div>
+                        {
+                            es() !== 'mobile' 
+                                ? 
+                                    <div className="footer_signature_container">
+                                        <div className="footer_signature playfair-display_light">
+                                            Adriano Gratani
+                                        </div>
+                                    </div>
+
+                                : null
+                        }
                     </div>
                     <div className="footer_links_addresses_container">
                         <h1 class="hello_title">
@@ -146,10 +166,31 @@ export default function FooterMain() {
                 </div>
             </div>
 
-            <div className="footer_footer_container">
-                <div>Copyright</div>
-                <div>Go to top</div>
-            </div>
+            {
+                            es() === 'mobile'
+                             
+                                ? 
+                                    <div className="footer_signature_container">
+                                        <div className="footer_signature playfair-display_light">
+                                            Adriano Gratani
+                                        </div>
+                                    </div>
+
+                                : null
+                        }
+
+
+            {
+                            es() !== 'mobile'
+
+                                ? 
+                                    <div className="footer_footer_container">
+                                        <div>Copyright</div>
+                                        <div>Go to top</div>
+                                    </div>
+
+                                : null
+            }
 
         </div>
     )
