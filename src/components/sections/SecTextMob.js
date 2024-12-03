020972a8e0e520826fb6d208d593ca5cdfb3235e
adriano <adriano.gratani1@gmail.com>
2024-12-03 19:28:45 +0900

    added logic for card removal on user click

--- src/components/sections/SecTextMob.js
+++ src/components/sections/SecTextMob.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import {useState , useEffect  } from 'react';
 import { useCardContext } from '../../context/CardContext';
-import { useScreenContext } from '../../context/ScreenSizeContext';
-import ExtMainland from '../extended_cards/extended_card_mainland/ExtMainland';
-import ExtSeaside from '../extended_cards/extended_card_seaside/ExtSeaside';
-import ExtIslets from '../extended_cards/extended_card_islets/ExtIslets';
 import "./sectextmob.css";
 
 export default function SecTextMob() {
-    
-    
     const { 
         cmi, setCmi,                  // for rendering/fading
         mc, setMc,                    // for data
-        currentlyVisitedSection,      // for layout
-
-        toggle_animation, setToggle_animation,
-        resetContextData
     } = useCardContext();
+
+    const [c, setC] = useState(0);   // 1  ?   call r() as effect;
+
+    function r() {         //  handle removal of card
+        setCmi(null);      // no clicked icons anymore in ctx
+        setMc(null);       // and no data.
+    }
+
+    useEffect(()=> {
+        if(c) {
+            r();
+        }
+    } ,[c])  ;
+    
+    if(cmi===null) return <div></div>   // base case
 
     switch(mc.s) {       // different layout (and DATA) for each location.
         case 'm': {      
             return (
-                <div className="stmc"
+                <div className="stmc" onClick={()=> setC(1)} // remove the card.
                 style={{display: 'flex', flexDirection: 'column', gap: '10px'}}
                 >
                     <h1 class="mh"
